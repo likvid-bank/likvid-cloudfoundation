919f13270f3398d20016bed54feee5d3c0c48e1d
Johannes Rudolph <jrudolph@meshcloud.io>
2023-11-23 10:45:07 +0100

    feat: teardown alz/base

--- kit/alz/base/modules/archetypes/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "configuration" {
-  value       = local.module_output
-  description = "Returns the archetype configuration data used to generate all resources needed to complete deployment of the Enterprise-scale Landing Zones, as per the specified archetype_id."
-}
