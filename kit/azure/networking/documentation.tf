68f20f9de1846392568aadd9da1e786717e428bd
Felix Zieger <fzieger@meshcloud.io>
2023-12-13 11:54:55 +0100

    docs: Heading for networking module

--- kit/azure/networking/documentation.tf
+++ kit/azure/networking/documentation.tf
@@ -1,5 +1,7 @@
 output "documentation_md" {
   value  = <<EOF
+# Networking
+
 Connection to the hub is the pre-requisite for getting access to th on-prem network.
 
 The hub itself has the following address space `${join(",",var.address_space)}`.
@@ -8,8 +10,9 @@ Upon request, we will peer a VNet in your subscription with the hub.
 
 We currently have assigned
 
-|Application|Range|
-|Glaskugel|10.1.0.0/24|
+| Application | Range |
+|-|-|
+| Glaskugel | 10.1.0.0/24 |
 
 Next free entry: 10.1.0.1/24
 EOF
