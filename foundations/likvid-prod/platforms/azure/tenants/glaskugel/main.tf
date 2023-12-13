a7fee89b6886ff779b2c39b430b882f03a0a98b6
Felix Zieger <fzieger@meshcloud.io>
2023-12-13 10:59:45 +0100

    chore: github links for readability

--- foundations/likvid-prod/platforms/azure/tenants/glaskugel/main.tf
+++ foundations/likvid-prod/platforms/azure/tenants/glaskugel/main.tf
@@ -1,18 +1,22 @@
 module "subscription" {
-  source = "../../../../../../../../../kit/azure/buildingblocks/subscription"
+  source = "github.com/likvid-bank/likvid-cloudfoundation/kit/azure/buildingblocks/subscription"
+  # Use local sources for testing
+  # source = "../../../../../../../../../kit/azure/buildingblocks/subscription"
 
   subscription_name       = "glaskugel"
   parent_management_group = "likvid-corp"
 }
 
 module "connectivity" {
+  source   = "github.com/likvid-bank/likvid-cloudfoundation/kit/azure/buildingblocks/connectivity"
+  # Use local sources for testing
+  # source   = "../../../../../../../../../kit/azure/buildingblocks/connectivity"
 
   providers = {
     azurerm.spoke = azurerm
     azurerm.hub   = azurerm.hub
   }
 
-  source   = "../../../../../../../../../kit/azure/buildingblocks/connectivity"
   location = "germanywestcentral"
   hub_rg   = "hub-vnet-rg"
   hub_vnet = "hub-vnet"
