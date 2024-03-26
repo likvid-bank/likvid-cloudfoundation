b80862c355ca6ca14fe732cd09f72276c2fe2061
Johannes Rudolph <jrudolph@meshcloud.io>
2024-03-26 18:48:41 +0100

    chore: skip installing azure cli on github actions

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem }:
 
 let
   # fake opentofu as terraform so that tools like terraform-docs pre-commit hook (which doesn't have tofu support)
@@ -6,7 +6,7 @@ let
   tofu_terraform =
     pkgs.stdenv.mkDerivation {
       name = "tofu-terraform";
-      phases = ["installPhase"];
+      phases = [ "installPhase" ];
       installPhase = ''
         mkdir -p $out/bin
         echo '#!/usr/bin/env sh' > $out/bin/terraform
@@ -23,11 +23,11 @@ let
     owner = "katexochen";
     repo = "nixpkgs";
     rev = "aacf05daec3141ce2bb34fd7c021a86401ac8c51";
-    sha256 = "11gdi69l7rdv0im4b1ka4b1gl9jiy6k4fzpk8hd44zss6w5ykhs4";     # update via nix-prefetch-url --unpack https://github.com/...
+    sha256 = "11gdi69l7rdv0im4b1ka4b1gl9jiy6k4fzpk8hd44zss6w5ykhs4"; # update via nix-prefetch-url --unpack https://github.com/...
   };
 
   # Import the fetched Nix expression
-  pkgsWithAzFixes = pkgs.callPackage pkgsWithAzFixesSrc {};
+  pkgsWithAzFixes = pkgs.callPackage pkgsWithAzFixesSrc { };
 
 in
 
@@ -47,8 +47,7 @@ pkgs.mkShell {
     # fake tofu as terraform
     tofu_terraform
 
-    # cloud provider clis
-    (pkgsWithAzFixes.azure-cli.withExtensions [ pkgsWithAzFixes.azure-cli.extensions.account ])
+
 
     # we currently don't have these managed by this collie repo, but will need it soon
     # pkgs.awscli2
@@ -58,5 +57,15 @@ pkgs.mkShell {
     pkgs.nodejs
 
     pkgs.pre-commit
-  ];
+  ] ++
+  (
+    # install the following tools only when not running in CI, because they are already preinstalled/expensive
+    if (builtins.getEnv "CI" == "true")
+    then 
+      [ ]
+    else
+      [
+        (pkgsWithAzFixes.azure-cli.withExtensions [ pkgsWithAzFixes.azure-cli.extensions.account ])
+      ]
+  );
 }
