44fe83e7fdcd742f7714af87788635b174fb65d9
florianow <64468897+florianow@users.noreply.github.com>
2024-04-30 19:24:15 +0200

    chore: pre-commit

--- kit/azure/landingzones/corp-online/main.tf
+++ kit/azure/landingzones/corp-online/main.tf
@@ -19,7 +19,7 @@ module "policy_corp" {
   template_file_variables = {
     default_location          = "${var.location}"
     connectivity_location     = "${var.location}"
-    current_scope_resource_id = azurerm_management_group.corp.id 
+    current_scope_resource_id = azurerm_management_group.corp.id
     root_scope_resource_id    = azurerm_management_group.corp.id
     vnet_address_space_id     = var.vnet_address_space_id
     private_dns_zone_prefix   = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/${var.cloudfoundation}/dns/providers/Microsoft.Network/privateDnsZones/"
