e13c6ec9f3b80f6cb9c428ed0a967f970fa3185d
florianow <64468897+florianow@users.noreply.github.com>
2024-05-13 14:35:00 +0200

    chore: adding mermaid diagram

--- kit/azure/buildingblocks/automation/documentation.tf
+++ kit/azure/buildingblocks/automation/documentation.tf
@@ -4,7 +4,7 @@ output "documentation_md" {
 
 The Automation Infrastructure Building Block configures the basic infrastructure for deploying a Building Block in a subscription. For this, we use a
 Service Principal to manage the resources in the subscription. This Service Principal is also used by meshStack.
-
+´´´mermaid
 graph LR
     A[${azuread_application.buildingblock.display_name}]
     C[meshStack]
@@ -13,6 +13,6 @@ graph LR
     A -->|Configures| E
     E -->|Deployed in| D
     C -->|Uses| A
-
+´´´
 EOF
 }
