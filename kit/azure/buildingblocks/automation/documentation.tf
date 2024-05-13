8259a0e5d3962d49d32de266343a9372bebdae75
florianow <64468897+florianow@users.noreply.github.com>
2024-05-13 11:39:39 +0200

    chore adding text for likvid-demo docu page for automation bb

--- kit/azure/buildingblocks/automation/documentation.tf
+++ kit/azure/buildingblocks/automation/documentation.tf
@@ -1,5 +1,9 @@
 output "documentation_md" {
   value = <<EOF
+# Building Block - Automation Infrastructure
+
+The Automation Infrastructure Building Block configures the basic infrastructure for deploying a Building Block in a subscription. For this, we use a
+Service Principal to manage the resources in the subscription. This Service Principal is also used by meshStack.
 
 EOF
 }
