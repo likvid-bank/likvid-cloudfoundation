a92a029f0bc6c7116b8318f64ffd547a0bf21949
florianow <64468897+florianow@users.noreply.github.com>
2024-05-16 15:42:35 +0200

    chore: added docu page for automation service principal

--- kit/azure/buildingblocks/automation/documentation.tf
+++ kit/azure/buildingblocks/automation/documentation.tf
@@ -1,5 +1,19 @@
 output "documentation_md" {
   value = <<EOF
 
+# 🏗️ Building Blocks Automation Infrastructure
+
+This module automates the deployment of building blocks within Azure. It utilizes service principles for automation. The states of these resources are maintained in a designated storage account.
+
+## 🛠️ Service Principal 
+
+- **ID**: `${azuread_service_principal.buildingblock.id}`
+- **Client ID**: `${azuread_service_principal.buildingblock.client_id}`
+
+## 🗃️ Storage Account
+
+- **Name**: `${azurerm_storage_account.tfstates.name}`
+- **Container Name**: `${azurerm_storage_container.tfstates.name}`
+
 EOF
 }
