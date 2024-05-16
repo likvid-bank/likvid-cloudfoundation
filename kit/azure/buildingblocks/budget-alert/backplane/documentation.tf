579b110ea8b1b2519e7f4de427dee1fde88f718b
florianow <64468897+florianow@users.noreply.github.com>
2024-05-16 16:31:25 +0200

    chore: adding better docu pages for buildingblocks

--- kit/azure/buildingblocks/budget-alert/backplane/documentation.tf
+++ kit/azure/buildingblocks/budget-alert/backplane/documentation.tf
@@ -8,5 +8,24 @@ mechanism to prevent unintentional cost overruns.
 
 We encourage application teams to deploy additional alerts with fine-grained notification rules according to the
 specific needs of their application and infrastructure.
+
+# 💰 Budget Alert Building Block Backplane
+
+This module automates the deployment of a Budget Alert building block within Azure. It utilizes service principles for automation. The states of these resources are maintained in a designated storage account.
+
+## 🛠️ Role Definition 
+
+- **NAME**: `${azurerm_role_definition.buildingblock_deploy.name}`
+- **ID**: `${azurerm_role_definition.buildingblock_deploy.id}`
+
+## 📝 Role Assignments
+
+- **IDs**: `${join(", ", [for id in azurerm_role_assignment.buildingblock_deploy : id.id])}`
+- **Principal IDs**: `${join(", ", [for id in azurerm_role_assignment.buildingblock_deploy : id.principal_id])}`
+
+## 🎯 Scope
+
+- **Scope**: `${var.scope}`
+
 EOF
 }
