57f787f85a0c08c485a424977f7c8980a31287aa
Felix Zieger <fzieger@meshcloud.io>
2024-08-09 13:11:49 +0200

    enable bigquery api

--- kit/gcp/data-lagoon/access/main.tf
+++ kit/gcp/data-lagoon/access/main.tf
@@ -1,3 +1,19 @@
-output "dataset_url" {
-  value = "https://console.cloud.google.com/bigquery?referrer=search&project=${var.project_id}&ws=!1m4!1m3!3m2!1sdata-lagoon-prod-45o!2sthelook"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.40.0"
+    }
+  }
 }
+
+provider "google" {
+  project = var.project_id
+}
+
+resource "google_project_service" "project" {
+  project            = var.project_id
+  service            = "bigquery.googleapis.com"
+  disable_on_destroy = true
+}
+
