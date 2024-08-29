4527162703f515de0486bc23d4e3667571b70eec
Felix Zieger <fzieger@meshcloud.io>
2024-08-29 16:34:05 +0200

    allow deletion of resources via building block deletion

--- kit/buildingblocks/starterkit/outputs.tf
+++ kit/buildingblocks/starterkit/outputs.tf
@@ -30,7 +30,11 @@ provider "github" {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false # This allows the deletion of the building block without having to separately delete the app resources
+    }
+  }
 
   resource_provider_registrations = "core"
 
