8d8832f382efe66cb2c76b9b532c016423f4a49f
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-09-03 16:44:40 +0200

    backplane description

--- kit/gcp/data-lagoon/buildingblock-backplane/documentation.tf
+++ kit/gcp/data-lagoon/buildingblock-backplane/documentation.tf
@@ -1,5 +1,5 @@
 output "documentation_md" {
   value = <<EOF
-This module manages the building block backend for GCP.
+This module manages the building block backend for GCP in the form of a service account with acccess on terraform backend storage and permissions to manage APIs for target projects.
 EOF
 }
