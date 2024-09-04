4d150e502c8caed5da418fd4cc7ab73fe71d5892
Johannes Rudolph <jrudolph@meshcloud.io>
2024-09-04 21:49:46 +0200

    refactor: remove unused keyvault reference from github-repo/backplane
    the unused error originally came up with tflint, so making the data
    useful was one idea to fix it, but turns out that terraform does not like
    interpolating sensitive values into non-sensitive output (for good reason)

--- kit/azure/buildingblocks/github-repo/backplane/documentation.tf
+++ kit/azure/buildingblocks/github-repo/backplane/documentation.tf
@@ -7,11 +7,6 @@ The Github Repository building block deploys a Github repository for the applica
 This building block is an essential part of the application infrastructure, enabling teams to focus on developing
 their application without worrying about the underlying repository setup.
 
-## Implementation Details
-
-For automation purposes, we store the PEM file for authenticating to GitHub API in Azure Key Vault as ${data.azurerm_key_vault_secret.github_token.name}.
-Right now, the secret needs to be manually set up and rotated.
-
-
+This building block utilizes the common [Azure Building Blocks Automation Infrastructure](./azure-buildingblocks-automation).
 EOF
 }
