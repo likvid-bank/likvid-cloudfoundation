6eb4ab97ad5ff80a38c83cdf82f6fb430ac96ccf
florianow <64468897+florianow@users.noreply.github.com>
2024-09-06 16:50:58 +0200

    chore: solve conflict

--- kit/aws/buildingblocks/iam-user/buildingblock/provider.tf
+++ kit/aws/buildingblocks/iam-user/buildingblock/provider.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
-    bucket = "buildingblocks-tfstates-yawi2"# Must match what's configured in automation backend
-=======
     bucket = "buildingblocks-tfstates-p32kj" # Must match what's configured in automation backend
->>>>>>> main
     key    = "terraform/iam-user"
     region = "eu-central-1"
   }
