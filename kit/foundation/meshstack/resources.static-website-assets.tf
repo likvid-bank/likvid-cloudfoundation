1f8cf8273285f7f52cef94718ed62aa801811161
florianow <64468897+florianow@users.noreply.github.com>
2024-09-25 11:13:27 +0200

    chore: change to prod workspaces for banking app and m25-platform

--- kit/foundation/meshstack/resources.static-website-assets.tf
+++ kit/foundation/meshstack/resources.static-website-assets.tf
@@ -13,14 +13,14 @@ locals {
 resource "meshstack_project" "static-website-assets" {
   provider = meshstack.static_website_assets
   metadata = {
-    name               = "static-website-assets-dev"
+    name               = "static-website-assets-prod"
     owned_by_workspace = "m25-platform"
   }
   spec = {
     display_name              = "Static Website Assets"
     payment_method_identifier = "m25-platform"
     tags = {
-      "environment"          = ["dev"]
+      "environment"          = ["prod"]
       "Schutzbedarf"         = ["public"]
       "LandingZoneClearance" = ["cloud-native"]
     }
@@ -35,7 +35,7 @@ resource "meshstack_tenant" "static-website-assets" {
     platform_identifier = "aws.aws-meshstack-dev"
   }
   spec = {
-    landing_zone_identifier = "likvid-aws-dev"
+    landing_zone_identifier = "likvid-aws-prod"
 
   }
 }
@@ -67,14 +67,14 @@ resource "meshstack_project_user_binding" "static_website_assets_project_admins"
 resource "meshstack_project" "m25_online_banking_app" {
   provider = meshstack.online_banking_app
   metadata = {
-    name               = "online-banking-app-dev"
+    name               = "online-banking-app-prod"
     owned_by_workspace = terraform_data.meshobjects_import["workspaces/m25-online-banki.yml"].output.metadata.name
   }
   spec = {
     display_name              = "Online Banking App"
     payment_method_identifier = "online-banking"
     tags = {
-      "environment"          = ["dev"]
+      "environment"          = ["prod"]
       "Schutzbedarf"         = ["public"]
       "LandingZoneClearance" = ["cloud-native"]
     }
@@ -89,7 +89,8 @@ resource "meshstack_tenant" "m25_online_banking_app" {
     owned_by_workspace  = terraform_data.meshobjects_import["workspaces/m25-online-banki.yml"].output.metadata.name
   }
   spec = {
-    landing_zone_identifier = "likvid-aws-dev"
+    landing_zone_identifier = "likvid-aws-prod"
+
   }
 }
 
