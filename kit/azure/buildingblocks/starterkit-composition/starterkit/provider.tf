2eeb3ba6f2c10c3d68e08b6d422fff3aacf6d826
Henry Dettmer <hdettmer@meshcloud.io>
2024-11-12 14:10:48 +0100

    docs: add starter kit guide

--- kit/azure/buildingblocks/starterkit-composition/starterkit/provider.tf
+++ kit/azure/buildingblocks/starterkit-composition/starterkit/provider.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     meshstack = {
       source  = "meshcloud/meshstack"
-      version = "0.5.3"
+      version = ">= 0.5.5"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "0.12.1"
     }
   }
 }
