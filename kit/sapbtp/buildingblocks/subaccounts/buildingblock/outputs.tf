dd0b581e371db8aa44a2edbbe2a97c705bc72aa2
Florian Nowarre <fnowarre@meshcloud.io>
2024-12-05 16:30:26 +0100

    adding output for the link

--- kit/sapbtp/buildingblocks/subaccounts/buildingblock/outputs.tf
+++ kit/sapbtp/buildingblocks/subaccounts/buildingblock/outputs.tf
@@ -9,3 +9,7 @@ output "btp_subaccount_region" {
 output "btp_subaccount_name" {
   value = btp_subaccount.subaccount.name
 }
+
+output "btp_subaccount_login_link" {
+  value = "https://emea.cockpit.btp.cloud.sap/cockpit#/globalaccount/${btp_subaccount.subaccount.parent_id}/subaccount/${btp_subaccount.subaccount.id}"
+}
