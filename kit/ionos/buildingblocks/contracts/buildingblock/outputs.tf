e739d422a1f507d8a88360227fc4c4b873eb285e
Florian Nowarre <fnowarre@meshcloud.io>
2024-12-18 14:36:49 +0100

    chore: remove outputs

--- kit/ionos/buildingblocks/contracts/buildingblock/outputs.tf
+++ kit/ionos/buildingblocks/contracts/buildingblock/outputs.tf
@@ -2,12 +2,12 @@ output "contract_id" {
   value = restapi_object.contract.id
 }
 
-output "password" {
-  value     = random_password.password.result
-  sensitive = true
-}
+# output "password" {
+#   value     = random_password.password.result
+#   sensitive = true
+# }
 
-output "username" {
-  value     = local.first_admin.email
-  sensitive = true
-}
+# output "username" {
+#   value     = local.first_admin.email
+#   sensitive = true
+# }
