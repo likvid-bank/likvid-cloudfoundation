770b6e6c42142e00c2bd53ce83935bcee3461c9d
Florian Nowarre <fnowarre@meshcloud.io>
2025-01-09 16:27:04 +0100

    chore: typo in documentation.tf

--- kit/stackit/bootstrap/documentation.tf
+++ kit/stackit/bootstrap/documentation.tf
@@ -3,7 +3,7 @@ output "documentation_md" {
 
 # STACKIT Cloud Custom Platform
 
-## Self-Service Project Provioning
+## Self-Service Project Provisioning
 
 At Likvid Bank, the Platform Team enables seamless self-service provisioning and management of STACKIT Projects for development teams. The platform is based on the STACKIT Cloud and is designed to i
 provide a secure and compliant environment for development teams to deploy and manage their applications.
