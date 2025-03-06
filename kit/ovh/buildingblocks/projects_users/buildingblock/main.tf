848244059296ddcf6cabefab30d0145e07da1f09
Florian Nowarre <fnowarre@meshcloud.io>
2025-03-06 16:10:29 +0100

    chore: adding only new users
    Feature/test pipeline bb

--- kit/ovh/buildingblocks/projects_users/buildingblock/main.tf
+++ kit/ovh/buildingblocks/projects_users/buildingblock/main.tf
@@ -37,8 +37,4 @@ resource "ovh_me_identity_user" "platform_users" {
   group       = each.value.group
   login       = each.value.login
   password    = random_password.user_passwords.result
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
