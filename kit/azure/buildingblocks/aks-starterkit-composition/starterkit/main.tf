d0fa0b931fc8d0a6e3787e16e0a04ed112df7e1d
Florian Nowarre <fnowarre@meshcloud.io>
2025-04-01 00:49:21 +0200

    chore: change to meshstack-Hub

--- kit/azure/buildingblocks/aks-starterkit-composition/starterkit/main.tf
+++ kit/azure/buildingblocks/aks-starterkit-composition/starterkit/main.tf
@@ -86,7 +86,7 @@ resource "meshstack_buildingblock" "github_actions_dev" {
     display_name = "GitHub Actions Connector"
     parent_building_blocks = {
       buildingblock_uuid = meshstack_building_block_v2.repo.metadata.uuid
-      definition_uuid    = meshstack_building_block_v2.spec.building_block_definition_version_ref.uuid
+      definition_uuid    = meshstack_building_block_v2.repo.spec.building_block_definition_version_ref.uuid
     }
     # inputs = {
     #   repo_name = { value_string = var.repo_name }
@@ -109,7 +109,7 @@ resource "meshstack_buildingblock" "github_actions_prod" {
     display_name = "GitHub Actions Connector"
     parent_building_blocks = {
       buildingblock_uuid = meshstack_building_block_v2.repo.metadata.uuid
-      definition_uuid    = meshstack_building_block_v2.spec.building_block_definition_version_ref.uuid
+      definition_uuid    = meshstack_building_block_v2.repo.spec.building_block_definition_version_ref.uuid
     } # inputs = {
     #   repo_name = { value_string = var.repo_name }
     # }
