8021193e6360f417d5d80fde2b1cb4b335261eb9
Florian Nowarre <fnowarre@meshcloud.io>
2025-07-03 11:14:43 +0200

    chore: sapbtp
    fix: use github-hosted png for the likvid bank logo
    
    chore: sapbtp
    
    fix: use github-hosted png for the likvid bank logo
    
    chore: sapbtp
    
    fix: use github-hosted png for the likvid bank logo
    
    chore: sapbtp
    
    fix: use github-hosted png for the likvid bank logo
    
    chore: sapbtp
    
    fix: use github-hosted png for the likvid bank logo

--- kit/sapbtp/buildingblocks/sapbtp-starterkit-composition/starterkit/main.tf
+++ kit/sapbtp/buildingblocks/sapbtp-starterkit-composition/starterkit/main.tf
@@ -84,15 +84,18 @@ resource "meshstack_buildingblock" "subaccount_dev" {
   depends_on = [meshstack_building_block_v2.subdirectory, time_sleep.wait_30_seconds]
   metadata = {
     definition_uuid    = "6214c14c-1bd5-46b1-a91f-7b0939219e4b"
-    definition_version = 38
+    definition_version = 43
     tenant_identifier  = "${meshstack_tenant.dev.metadata.owned_by_workspace}.${meshstack_tenant.dev.metadata.owned_by_project}.${meshstack_tenant.dev.metadata.platform_identifier}"
   }
   spec = {
     display_name = "subaccount ${var.project_identifier}-dev"
-    parent_building_blocks = [{
-      buildingblock_uuid = meshstack_building_block_v2.subdirectory.metadata.uuid
-      definition_uuid    = "4ccc9715-f773-4c23-87e0-ae5fa4899176"
-    }]
+    # parent_building_blocks = [{
+    #   buildingblock_uuid = meshstack_building_block_v2.subdirectory.metadata.uuid
+    #   definition_uuid    = "4ccc9715-f773-4c23-87e0-ae5fa4899176"
+    # }]
+    inputs = {
+      subfolder = { value_string = var.project_identifier }
+    }
   }
 }
 
@@ -100,14 +103,16 @@ resource "meshstack_buildingblock" "subaccount_prod" {
   depends_on = [meshstack_building_block_v2.subdirectory, time_sleep.wait_30_seconds]
   metadata = {
     definition_uuid    = "6214c14c-1bd5-46b1-a91f-7b0939219e4b"
-    definition_version = 38
+    definition_version = 43
     tenant_identifier  = "${meshstack_tenant.prod.metadata.owned_by_workspace}.${meshstack_tenant.prod.metadata.owned_by_project}.${meshstack_tenant.prod.metadata.platform_identifier}"
   }
   spec = {
     display_name = "subaccount ${var.project_identifier}-prod"
-    parent_building_blocks = [{
-      buildingblock_uuid = meshstack_building_block_v2.subdirectory.metadata.uuid
-      definition_uuid    = "4ccc9715-f773-4c23-87e0-ae5fa4899176"
-    }]
+    # parent_building_blocks = [{
+    #   buildingblock_uuid = meshstack_building_block_v2.subdirectory.metadata.uuid
+    #   definition_uuid    = "4ccc9715-f773-4c23-87e0-ae5fa4899176"
+    inputs = {
+      subfolder = { value_string = var.project_identifier }
+    }
   }
 }
