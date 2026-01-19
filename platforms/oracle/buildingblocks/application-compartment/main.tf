9b96a25965340bfd48399fb0c061cbc6870baa74
Florian Nowarre <fnowarre@meshcloud.io>
2026-01-19 14:55:17 +0100

    feat: usermap wrong

--- platforms/oracle/buildingblocks/application-compartment/main.tf
+++ platforms/oracle/buildingblocks/application-compartment/main.tf
@@ -1,9 +1,18 @@
+data "oci_identity_users" "all_users" {
+  compartment_id = var.tenancy_ocid
+}
+
 locals {
   compartment_name = "${var.foundation}-${var.workspace_id}-${var.project_id}"
 
-  admin_users  = { for user in var.users : user.euid => user if contains(user.roles, "admin") }
-  user_users   = { for user in var.users : user.euid => user if contains(user.roles, "user") && !contains(user.roles, "admin") }
-  reader_users = { for user in var.users : user.euid => user if contains(user.roles, "reader") && !contains(user.roles, "admin") && !contains(user.roles, "user") }
+  user_ocid_map = {
+    for user in data.oci_identity_users.all_users.users :
+    user.email => user.id
+  }
+
+  admin_users  = { for user in var.users : user.email => lookup(local.user_ocid_map, user.email, null) if contains(user.roles, "admin") && lookup(local.user_ocid_map, user.email, null) != null }
+  user_users   = { for user in var.users : user.email => lookup(local.user_ocid_map, user.email, null) if contains(user.roles, "user") && !contains(user.roles, "admin") && lookup(local.user_ocid_map, user.email, null) != null }
+  reader_users = { for user in var.users : user.email => lookup(local.user_ocid_map, user.email, null) if contains(user.roles, "reader") && !contains(user.roles, "admin") && !contains(user.roles, "user") && lookup(local.user_ocid_map, user.email, null) != null }
 }
 
 resource "oci_identity_compartment" "application" {
@@ -34,21 +43,21 @@ resource "oci_identity_user_group_membership" "readers" {
   for_each = local.reader_users
 
   group_id = oci_identity_group.readers.id
-  user_id  = each.value.euid
+  user_id  = each.value
 }
 
 resource "oci_identity_user_group_membership" "users" {
   for_each = local.user_users
 
   group_id = oci_identity_group.users.id
-  user_id  = each.value.euid
+  user_id  = each.value
 }
 
 resource "oci_identity_user_group_membership" "admins" {
   for_each = local.admin_users
 
   group_id = oci_identity_group.admins.id
-  user_id  = each.value.euid
+  user_id  = each.value
 }
 
 resource "oci_identity_policy" "application" {
