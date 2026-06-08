f3ae39192f875b05f48c0ee822c5e959c9b7cf80
Fabian Muscariello <fmuscariello@meshcloud.io>
2026-06-08 13:15:40 +0200

    fix: bump meshstack terraform provider to 0.21.0

--- foundations/likvid-prod/platforms/ske/starterkit/versions.tf
+++ foundations/likvid-prod/platforms/ske/starterkit/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     meshstack = {
       source  = "meshcloud/meshstack"
-      version = "~> 0.20.0"
+      version = "~> 0.21.0"
     }
     stackit = {
       source  = "stackitcloud/stackit"
